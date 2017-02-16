df7e9490670b448d474030496e024483cd8cb896
igotyooo <overmars29@gmail.com>
2017-02-16 22:50:48 +0000

    db changed from 10fps to 25fps

--- setpath.lua
+++ setpath.lua
@@ -1,5 +1,5 @@
 gpath = { db = {  }, net = {  } }
-gpath.db.ucf101 = '/home/dgyoo/workspace/datain/UCF101FRAME/'
+gpath.db.ucf101_rgb_s1 = '/home/dgyoo/workspace/datain/UCF101_25FPS/'
 gpath.net.alex_caffe_model = '/home/dgyoo/workspace/net/caffe/bvlc_alexnet.caffemodel'
 gpath.net.alex_caffe_proto = '/home/dgyoo/workspace/net/caffe/bvlc_alexnet_deploy.prototxt'
 gpath.net.vggm_caffe_model = '/home/dgyoo/workspace/net/caffe/VGG_CNN_M_2048.caffemodel'
